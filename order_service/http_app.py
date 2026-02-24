c74bb22feecc31783cfd78efba8cdb2e84899a0d
ardt <arrdt11@gmail.com>
2026-02-24 11:43:53 +0000

    Add GET /orders/{id} endpoint

--- order_service/http_app.py
+++ order_service/http_app.py
@@ -1,27 +1,49 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
 import requests
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from order_service.storage import orders
 
 app = FastAPI()
+
 
 class OrderRequest(BaseModel):
     user_id: int
     amount: float
 
+
 @app.post("/orders")
 def create_order(order_request: OrderRequest):
+
     order_id = len(orders) + 1
+
     orders[order_id] = {
         "id": order_id,
         "user_id": order_request.user_id,
         "amount": order_request.amount,
         "status": "created"
     }
-    response = requests.post("http://127.0.0.1:8001/pay", json={
-        "order_id": order_id,
-        "amount": order_request.amount
-    })
+
+    response = requests.post(
+        "http://127.0.0.1:8001/pay",
+        json={
+            "order_id": order_id,
+            "amount": order_request.amount
+        }
+    )
+
     result = response.json()
+
     orders[order_id]["status"] = result["status"]
+
     return orders[order_id]
+
+
+@app.get("/orders/{order_id}")
+def get_order(order_id: int):
+
+    order = orders.get(order_id)
+
+    if order is None:
+        raise HTTPException(status_code=404, detail="Order not found")
+
+    return order
